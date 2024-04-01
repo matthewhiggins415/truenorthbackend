06d2c6a65c384e455ddb06ccf81d771647ce04f8
matthewhiggins415 <mhiggins.louis@gmail.com>
2024-04-01 12:07:56 -0700

    crud job resource working.

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -21,7 +21,6 @@ router.post('/create-job', requireToken, async (req, res, next) => {
 // read all jobs of contact
 router.get('/get-jobs/:id', requireToken, async (req, res, next) => {
   const { id } = req.params;
-  console.log("id: ", id);
   
   try {
     const jobs = await Job.find({ contact_id: id }).exec();
@@ -33,9 +32,54 @@ router.get('/get-jobs/:id', requireToken, async (req, res, next) => {
 });
 
 // read single job of contact
+router.get('/get-job/:id', requireToken, async (req, res, next) => {
+  const { id } = req.params;
+    
+  try {
+    const job = await Job.findById(id)
+    res.json({ job });
+  } catch(error) {
+    console.log(error);
+    res.status(500).json({ error: 'Failed to retrieve jobs' });
+  }
+});
 
 // update single job
+router.put('/update-job/:id', requireToken, async (req, res, next) => {
+  const jobID = req.params.id;
+  const updatedJob = req.body.job;
+
+  console.log(updatedJob)
+  console.log(jobID)
+  
+  try {
+    let job = await Job.findById(jobID);
+  
+    // Update contact fields if they exist in the updatedContact object
+    Object.keys(updatedJob).forEach(key => {
+      if (updatedJob[key]) {
+        job[key] = updatedJob[key];
+      }
+    });
+  
+    const newJob = await job.save();
+      
+    res.status(201).json({ newJob });
+  } catch(error) {
+    res.status(500).json({ msg: 'Something went wrong' });
+  }
+});
 
 // delete job
+router.delete('/jobs/:id', requireToken, async (req, res, next) => {
+  const id = req.params.id;
+  console.log(id)
+  try {
+    let job = await Job.findByIdAndDelete(id);
+    res.status(204).json({ msg: 'job deleted' })
+  } catch(e) {
+    res.json({ msg: 'something went wrong'})
+  }
+})
 
 module.exports = router;
